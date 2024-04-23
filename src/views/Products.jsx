0d50f3cfdbb667f3e05079469a4cd8e574585d63
juliana Zamora <fatimajuliana.zamoramichel@umg.edu.mx>
2024-04-22 21:53:18 -0600

    first commit

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ProductCard from '../components/ProductCard';
+import { useNavigate } from "react-router-dom";
 
 function Products() {
+    const [data, setData] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:8000/api/floreria/")
+            .then((response) => {
+                console.log(response.data);
+                setData(response.data);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }, []);
+
+    console.log(data);
+
     return (
-    <div> Products </div>
-    )
+        <div>
+            {data ? (
+                data.map(item => (
+                    <ProductCard
+                        nombre={item.nombre} 
+                        color={item.color}  
+                        precio={item.precio}
+                        onClick={() => {
+                            console.log(item.id); // Imprime el ID real del elemento
+                            navigate(`producto/${item.id}`);
+                        }}
+                    />
+                ))
+            ) : (
+                <p>Esperando elementos...</p>
+            )}
+        </div>
+    );
 }
 
-export default Products
+export default Products;
